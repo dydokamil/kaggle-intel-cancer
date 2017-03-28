c14078a032ee5d30f95959c23a1b55cb9c1d3cb2
dydokamil <dydokamil@gmail.com>
2017-03-28 14:05:41 +0200

    Split data into train/valid

--- tools.py
+++ tools.py
@@ -1,21 +1,40 @@
 import os
+from PIL import Image
+import numpy as np
 import random
-
+import tensorflow as tf
 import matplotlib.pyplot as plt
 
 DATA_PATH = r"/media/hdd/training_data/intel-cancer/Screening"
+dirs = ['Type_1', 'Type_2', 'Type_3']
 TRAIN_PATH = os.path.join(DATA_PATH, 'train')
 TEST_PATH = os.path.join(DATA_PATH, 'test')
+VALID_PATH = os.path.join(DATA_PATH, 'valid')
 
 
 def sample_image():
-    dirs = ['Type_1', 'Type_2', 'Type_3']
     sample_dir = random.sample(dirs, 1)[0]
     sample = random.sample(os.listdir(os.path.join(TRAIN_PATH, sample_dir)), 1)[0]
     sample = os.path.join(TRAIN_PATH, sample_dir, sample)
     img = plt.imread(sample)
-    plt.imshow(img)
-    plt.show()
+    return img
 
 
-sample_image()
+def init_paths():
+    train_paths = []
+    valid_paths = []
+    for dir in dirs:
+        type_path_train = os.path.join(TRAIN_PATH, dir)
+        type_path_valid = os.path.join(VALID_PATH, dir)
+        files_train = os.listdir(type_path_train)
+        files_valid = os.listdir(type_path_valid)
+        train_paths.extend([os.path.join(type_path_train, file) for file in files_train])
+        valid_paths.extend([os.path.join(type_path_valid, file) for file in files_valid])
+    return train_paths, valid_paths
+
+
+def input_producer(train_paths):
+    pass
+
+
+train_paths, valid_paths = init_paths()
